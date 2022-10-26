a419106c08f7d59a7e1ee07d29d1db60dab53228
Ragan Hagey <raganahagey@gmail.com>
2022-10-26 17:52:46 -0400

    new branch

--- tummygucci-app/src/components/CreatureContainer.js
+++ tummygucci-app/src/components/CreatureContainer.js
@@ -2,23 +2,23 @@ import React, {useState} from "react";
 
 export default function CreatureContainer(props){
 
-    //Setter for showing the thought bubble container based on mood/hunger. Bubble should be hidden on hunger/mood 5+
+    //Setter for showing the thought bubble container based on mood/hunger. 
     const [showBubble, setShowBubble] = useState(false)
 
     //Setter for showing the creature images based on mood/hunger
     const [showCreature, setShowCreature] = useState(true)
 
-
+    //Bubble should be hidden on hunger/mood 5+
     //SHOW BUBBLE FUNCTION START***//
     function bubbleStatus (){
-
+        
     }
     //SHOW BUBBLE FUNCTION STOP***//
 
     //**CHARACTER SWAP FUNCTION START**
     //if mood <4 show ./images/Angry.gif
     //if mood 5-7 show ./images/neutral.gif
-    //if mood 5-7 show ./images/happy.gif
+    //if mood 7+ show ./images/happy.gif
     function charStatus (){
 
     }
